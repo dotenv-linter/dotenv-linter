8cdc9714848bb80d3025fcef901dda9618a012c9
Mikhail Grachev <work@mgrachev.com>
2019-12-27 22:21:01 +0300

    Add support for command line arguments
    Signed-off-by: Mikhail Grachev <work@mgrachev.com>

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
+extern crate clap;
 extern crate dotenv_linter;
 
 use std::process;
 
 fn main() {
+    clap::App::new(env!("CARGO_PKG_NAME"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .version_short("v")
+        .get_matches();
+
     if let Err(e) = dotenv_linter::run() {
         eprintln!("Error: {}", e);
         process::exit(1);
