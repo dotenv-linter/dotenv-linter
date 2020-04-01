f3e2c45b42ba40a675a23cdb3b8b2532f3f696d2
Christopher Morton <sonro@gmx.com>
2020-04-01 11:29:55 +0100

    Add test coverage for --exclude argument (#135)
    Fixes #127

--- tests/cli_test.rs
+++ tests/cli_test.rs
@@ -268,3 +268,73 @@ fn exits_with_0_on_no_errors() {
     drop(file);
     current_dir.close().unwrap();
 }
+
+#[test]
+fn exclude_one_file() {
+    let current_dir = tempdir().unwrap();
+    let file_path = current_dir.path().join(".env");
+    let file_path_str = file_path.to_str().unwrap();
+    let mut file = File::create(&file_path).unwrap();
+    writeln!(file, " FOO=").unwrap();
+
+    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+    cmd.current_dir(&current_dir)
+        .args(&["--exclude", file_path_str])
+        .assert()
+        .success();
+
+    drop(file);
+    current_dir.close().unwrap();
+}
+
+#[test]
+fn exclude_two_files() {
+    let current_dir = tempdir().unwrap();
+    let file_1_path = current_dir.path().join(".env");
+    let file_1_path_str = file_1_path.to_str().unwrap();
+    let mut file_1 = File::create(&file_1_path).unwrap();
+    writeln!(file_1, " FOO=").unwrap();
+
+    let file_2_path = current_dir.path().join(".local.env");
+    let file_2_path_str = file_2_path.to_str().unwrap();
+    let mut file_2 = File::create(&file_2_path).unwrap();
+    writeln!(file_2, " BAR=").unwrap();
+
+    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+    cmd.current_dir(&current_dir)
+        .args(&["-e", file_1_path_str, "-e", file_2_path_str])
+        .assert()
+        .success();
+
+    drop(file_1);
+    drop(file_2);
+    current_dir.close().unwrap();
+}
+
+#[test]
+fn exclude_one_file_check_one_file() {
+    let current_dir = tempdir().unwrap();
+    let file_to_check_path = current_dir.path().join(".env");
+    let mut file_to_check = File::create(&file_to_check_path).unwrap();
+    writeln!(file_to_check, " FOO=").unwrap();
+
+    let file_to_exclude_path = current_dir.path().join(".exclude-me.env");
+    let mut file_to_exclude = File::create(&file_to_exclude_path).unwrap();
+    writeln!(file_to_exclude, " BAR=").unwrap();
+    let file_to_exclude_str = file_to_exclude_path.to_str().unwrap();
+
+    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+    cmd.current_dir(&current_dir)
+        .args(&["--exclude", file_to_exclude_str])
+        .assert()
+        .failure()
+        .code(1)
+        .stdout(format!(
+            "{}:1 Invalid leading character detected\n",
+            file_to_check_path.file_name().unwrap().to_str().unwrap()
+        ));
+
+    drop(file_to_exclude);
+    drop(file_to_check);
+    current_dir.close().unwrap();
+}
