75527858da559da46dfbadfe659cfd6013d7a332
tyler <baile320@umn.edu>
2020-08-14 09:57:18 -0500

    add test for remove_all_invalid_leading_chars

--- src/common.rs
+++ src/common.rs
@@ -193,6 +193,15 @@ mod tests {
         );
     }
 
+    #[test]
+    fn remove_all_invalid_leading_chars_test() {
+        let string = String::from("-1&*FOO");
+        assert_eq!("FOO", remove_all_invalid_leading_chars(&string));
+
+        let string = String::from("***FOO-BAR");
+        assert_eq!("FOO-BAR", remove_all_invalid_leading_chars(&string));
+    }
+
     mod file_entry {
         use super::*;
 
