e658c09ba2c207ff97a6cde57ddce53fbf5f8275
tyler <baile320@umn.edu>
2020-08-20 07:16:14 -0500

    borrow variable instead of recreating it

--- src/checks/incorrect_delimiter.rs
+++ src/checks/incorrect_delimiter.rs
@@ -34,11 +34,7 @@ impl Check for IncorrectDelimiterChecker<'_> {
             .chars()
             .any(|c| !c.is_alphanumeric() && c != '_')
         {
-            return Some(Warning::new(
-                line.clone(),
-                self.name(),
-                self.message(&line.get_key()?),
-            ));
+            return Some(Warning::new(line.clone(), self.name(), self.message(&key)));
         }
 
         None
