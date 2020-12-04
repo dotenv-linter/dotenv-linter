40885ba6e24a4f5e54d939c81b1323a055c0fbe2
Valentin Brandl <mail+dev@vbrandl.net>
2020-12-04 15:13:17 +0100

    feat: implement test for new change

---
+++ tests/checks/key_without_value.rs
@@ -0,0 +1,21 @@
+use crate::common::*;
+
+#[test]
+fn also_detect_lowercase_key() {
+    let contents = vec!["test\n"];
+
+    for contents in contents {
+        let testdir = TestDir::new();
+        let testfile = testdir.create_testfile(".env", contents);
+        let args = &[testfile.as_str()];
+        let expected_output = check_output(&[(
+            ".env",
+            &[
+                ".env:1 KeyWithoutValue: The test key should be with a value or have an equal sign",
+                ".env:1 LowercaseKey: The test key should be in uppercase",
+            ],
+        )]);
+
+        testdir.test_command_fail_with_args(args, expected_output);
+    }
+}
--- tests/checks/mod.rs
+++ tests/checks/mod.rs
@@ -1,3 +1,4 @@
 mod ending_blank_line;
 mod extra_blank_line;
+mod key_without_value;
 mod quote_character;
