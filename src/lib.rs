34ffe731712c8f0acc3944a9243fd801df2edbdc
Marcel Baur <marcel.baur@tawny.ai>
2021-04-09 19:54:42 +0200

    feat: affirm number of fixes done match expected number of fixes todo

--- src/lib.rs
+++ src/lib.rs
@@ -68,9 +68,13 @@ pub fn fix(args: &clap::ArgMatches, current_dir: &Path) -> Result<(), Box<dyn Er
 
         let mut lines = get_line_entries(&fe, strings);
         let mut result = checks::run(&lines, &skip_checks);
-
+        let expected_fixes = result.len();
+        let fixes_done = fixes::run(&mut result, &mut lines, &skip_checks);
+        if fixes_done < expected_fixes {
+            print!("Could not fix all warnings!");
+        }
         // run fixers & write results to file
-        if !result.is_empty() && fixes::run(&mut result, &mut lines, &skip_checks) > 0 {
+        if !result.is_empty() && fixes_done > 0 {
             let should_backup = !args.is_present("no-backup");
             // create backup copy unless user specifies not to
             if should_backup {
