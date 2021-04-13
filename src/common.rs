55e8dba9dab317ccbd7bc3d077f2a464100ee8d4
mc1098 <m.cripps1@uni.brighton.ac.uk>
2021-04-13 08:30:03 +0100

    docs: add documentation for check_tester macro
    Internal documentation for the check_tester macro to make it
    easier to understand and use with some examples.

--- src/common.rs
+++ src/common.rs
@@ -26,6 +26,75 @@ pub(crate) mod tests {
     use std::path::PathBuf;
     use std::rc::Rc;
 
+    /**
+       Creates test cases for Check trait implementations.
+
+       check_tester takes the name of the Check implementation which is
+       followed by the test cases.
+
+       Each test case is the same as a test function so multiple inputs
+       will occur on the same instance of the Check implementation, allowing
+       the implementation to use internal state (if required).
+
+       # Examples
+
+       The following example shows a single test case for the
+       DuplicatedKeyChecker:
+
+       ```
+       check_tester!{
+           DuplicatedKeyChecker;
+           duplicated_key_test => {
+               // First time seeing key "FOO" so is None
+               "FOO=BAR" => None,
+               // Second time seeing key "FOO" so is duplicate.
+               "FOO=BAR" => Some("The FOO key is duplicated"),
+           }
+       }
+       ```
+
+       The above example could be expanded as such:
+       ```
+        #[test]
+        fn duplicated_key_test() {
+            let mut checker = DuplicatedKeyChecker::default();
+
+            assert_eq!(None, checker.run(&line_entry(1, 2, "FOO=BAR")));
+            assert_eq!(Some(
+                Warning::new(
+                    line_entry(2, 2, "FOO=BAR")),
+                    "DuplicatedKey",
+                    "The FOO key is duplicated"
+                ),
+                checker.run(&line_entry(2, 2, "FOO=BAR"))
+            );
+        }
+       ```
+       As shown in the expanded example this macro will create the correct
+       Line_Entry for the checker and will also create the expected Warning
+       using the input and expected warning message.
+
+       ## Constant Warning Message
+
+       Note that the expected warning message does not need to be a string
+       literal; in the case where the warning message is always the same
+       a constant &str can be used:
+
+       ```
+        const WARNING: &str = "Invalid leading character detected";
+
+        check_tester!{
+            LeadingCharacterChecker;
+            leading_dot_is_not_valid => {
+                ".FOO=BAR" => Some(WARNING),
+            },
+            leading_number_is_not_valid => {
+                "1FOO=BAR" => Some(WARNING),
+            }
+        }
+       ```
+    */
+    #[cfg(test)]
     #[macro_export]
     macro_rules! check_tester {
         (@token $t:tt $sub:expr) => {$sub};
