34aad3167b20f0ab33e87ef627aab69b9a0d3625
Poxxy <41162160+Poxxy@users.noreply.github.com>
2023-03-16 08:59:40 -0400

    feat: add "No difference found" message on compare
    Add "No difference found" message when comparing two files which have no difference.

--- dotenv-linter/tests/compare/compare.rs
+++ dotenv-linter/tests/compare/compare.rs
@@ -5,7 +5,7 @@ fn files_with_same_environment_variables() {
     let test_dir = TestDir::new();
     let testfile_one = test_dir.create_testfile(".env1", "FOO=abc\nBAR=def");
     let testfile_two = test_dir.create_testfile(".env2", "FOO=abc\nBAR=def");
-    let expected_output = "Comparing .env1\nComparing .env2\n";
+    let expected_output = "Comparing .env1\nComparing .env2\nNo difference found\n";
 
     test_dir.test_command_success_with_args(
         &["compare", testfile_one.as_str(), testfile_two.as_str()],
