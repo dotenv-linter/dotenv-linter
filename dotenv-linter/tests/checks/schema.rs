b1438637e6d691f0d149f6fa6e272fa888e01a9c
paul moore <paulmoore100@hotmail.com>
2024-03-09 09:54:33 -0800

    change tempfile handling

--- dotenv-linter/tests/checks/schema.rs
+++ dotenv-linter/tests/checks/schema.rs
@@ -58,22 +58,17 @@ fn load_bad_schema() {
         bad_json
     }"#;
     // write the above json to a temp file
-    let temp_dir = tempdir().unwrap();
-    let file_path = temp_dir.path().join("schema.json");
-    {
-        let mut file = File::create(&file_path).unwrap();
-        file.write_all(json.as_bytes()).unwrap();
-    }
-    let content = "NAME=JOE\n";
 
     let testdir = TestDir::new();
+    let test_schema = testdir.create_testfile("schema.json", json);
+
+    let content = "NAME=JOE\n";
     let testfile = testdir.create_testfile(".env", content);
-    let args = &["-S", file_path.to_str().unwrap(), testfile.as_str()];
+    let args = &["-S", test_schema.as_str(), testfile.as_str()];
 
     let expected_output = "Error loading schema: key must be a string at line 3 column 9\n";
 
     testdir.test_command_fail_with_args(with_default_args(args), expected_output);
-    let _ = remove_file(file_path);
 }
 #[test]
 fn load_missing_schema() {
@@ -85,7 +80,7 @@ fn load_missing_schema() {
     let expected_output = if cfg!(target_os = "windows") {
         "Error loading schema: The system cannot find the file specified. (os error 2)\n"
     } else {
-        "Error loading schema: The system cannot find the file specified. (os error 2)\n"
+        "Error loading schema: No such file or directory (os error 2)\n"
     };
     testdir.test_command_fail_with_args(with_default_args(args), expected_output);
 }
