f8a7552bc8b35f76f5389a03c989e313f306af18
Grachev Mikhail <work@mgrachev.com>
2025-10-18 16:33:00 +0300

    chore: fix release script (#923)

--- release.sh
+++ release.sh
@@ -12,14 +12,14 @@ git checkout -b "release-${branch_name}"
 
 # Update the version
 msg="# managed by release.sh"
-sed -i '' "s/^version = .* $msg$/version = \"${1#v}\" $msg/" dotenv-linter/Cargo.toml
+sed -i '' "s/^version = .* $msg$/version = \"${1#v}\" $msg/" dotenv-cli/Cargo.toml
 
 # Update the changelog
 git-cliff --tag "$1" --unreleased --prepend CHANGELOG.md
 
 # Commit changes
 cargo check
-git add CHANGELOG.md dotenv-linter/Cargo.toml Cargo.lock
+git add CHANGELOG.md dotenv-cli/Cargo.toml Cargo.lock
 git commit -m "chore(release): $1"
 
 echo "Done."
